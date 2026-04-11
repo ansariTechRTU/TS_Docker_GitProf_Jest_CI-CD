57edaa5b93b4e601672b286bf6679078b619a538
Abdullah Ansari <ansari.tech.rtu@gmail.com>
2026-04-11 12:28:53 +0200

    basics of func & obj in TS

---
+++ TS/intro.js
@@ -0,0 +1,35 @@
+// function addTwo(num){    //any type -problematic
+//     return num+2
+// }
+// addTwo(5);
+//fix
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(5);
+//also
+function signUpUser(name, email, isPaid) {
+}
+signUpUser("adam", "adam@gmail.com", true);
+//wanna give default value
+let loginUser = (name, email, isPaid = false) => { };
+loginUser("h", "h@gmail.com");
+//one more fix to addtwo func (adding return type should be only number)
+function addTwoNew(num) {
+    //return "hello"   //this was possible if we dont annotate the return value to be only number
+    return num + 2;
+}
+addTwo(5);
+//also TS automatically get aware of context --here it already knows its string, no need to tell
+const heros = ["spidy", "superman", "antman"];
+heros.map((hero) => {
+    return `my fav hero is ${hero}`;
+});
+//void to tell no return
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
+export {};
---
+++ TS/object.ts
@@ -0,0 +1,19 @@
+const User = {
+    name: "abd",
+    email: "abd@gmail.com",
+    isActive: true
+}
+
+
+
+function createUser(user: {name: string, isPaid: boolean}){}
+
+createUser({name: "adam" , isPaid: false})
+
+
+
+
+
+
+
+export{}
