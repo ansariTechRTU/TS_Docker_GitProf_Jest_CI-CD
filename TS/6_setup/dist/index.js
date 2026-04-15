ac70774748dbf22384866c38ea05891ab9899dfe
Abdullah Ansari <ansari.tech.rtu@gmail.com>
2026-04-15 19:37:31 +0200

    public/private access modifiers + getters/setters

--- TS/6_setup/dist/index.js
+++ TS/6_setup/dist/index.js
@@ -1,4 +1,15 @@
 "use strict";
-// Object.defineProperty(exports, "__esModule", { value: true });
-console.log("TS is here");
+//Object.defineProperty(exports, "__esModule", { value: true });
+class User {
+    email;
+    name;
+    city = '';
+    constructor(email, name) {
+        this.email = email,
+            this.name = name;
+    }
+}
+const myself = new User("me@mail.com", "ansari");
+console.log(myself);
+myself.city = "Riga";
 //# sourceMappingURL=index.js.map
--- TS/6_setup/src/index.ts
+++ TS/6_setup/src/index.ts
@@ -1,1 +1,36 @@
-console.log("TS is here");
+class User {
+     
+    private course_count =1
+
+    public email: string
+    name: string
+    private readonly city: string = ''        //private -means u can access within obj only
+    constructor(email: string , name: string){
+        this.email = email,
+        this.name=name
+    }
+
+    get getAppleEmail(): string{    //getter
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this.course_count
+
+    }
+
+    set courseCount(courseNum) {     //setter
+       if(courseNum<=1){
+        throw new Error("course count should be more than one")
+       }
+       this.course_count = courseNum
+    }
+
+
+
+
+}
+const myself = new User("me@mail.com" , "ansari")
+console.log(myself)
+
+// myself.city="Riga"
